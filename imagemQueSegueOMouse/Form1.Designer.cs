5f840b6eeadbeff48b6ed0a7c228d2800e1403f8
Galboltan <vivito61@gmail.com>
2020-09-07 09:51:13 -0300

    Nome do button e form adicionados

--- imagemQueSegueOMouse/Form1.Designer.cs
+++ imagemQueSegueOMouse/Form1.Designer.cs
@@ -42,7 +42,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -69,7 +69,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Gado_Stalker";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
